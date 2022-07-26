73d446df7fa46b6bb50a653010f28a95dfdbf38e
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-07-26 21:16:37 +0500

    test3_1

--- test3.py
+++ test3.py
@@ -5,8 +5,11 @@ class Host:
     def set_name(self, new_name):
         self._name = new_name
 
+    def get_name(self):
+        return self._name
+
 
 user1 = Host('Jhon')
-print(user1._name)
+print(user1.get_name())
 user1.set_name('Artur')
-print(user1._name)
+print(user1.get_name())
